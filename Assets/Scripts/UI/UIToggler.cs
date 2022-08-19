8e1be28c745891ed33e4ec528f29d7e0992e7ae5
limtingying limtingying <ltyguydev@gmail.com>
2022-08-19 23:38:18 +0800

    UIToggler added for quick testing

---
+++ Assets/Scripts/UI/UIToggler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIToggler : MonoBehaviour
+{
+    public GameObject ui;                       // To get the panel
+
+    public void TogglePanel()
+    {
+        if (ui == null)
+        {
+            return;
+        }
+
+        bool isActive = ui.activeSelf;
+
+        SetPanelTo(!isActive);
+    }
+
+    public void SetPanelTo(bool a)
+    {
+        if (ui == null)
+        {
+            return;
+        }
+        ui.SetActive(a);
+    }
+}
