8d65dbf7928088bdd4b7b99bf3349edaa708016c
winstxnhdw <winstxnhdw@gmail.com>
2022-08-20 00:01:16 +0800

    update

---
+++ Assets/Scripts/UI/Helpers/PanelToggler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PanelToggler : MonoBehaviour {
+    [SerializeField]
+    GameObject panel;
+
+    public void TogglePanel() {
+        if (this.panel == null) return;
+        SetPanelTo(!this.panel.activeSelf);
+    }
+
+    public void SetPanelTo(bool a) {
+        if (this.panel == null) return;
+        this.panel.SetActive(a);
+    }
+}
